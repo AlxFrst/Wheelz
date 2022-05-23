0b14258ca203689820d71832d0bbab4b63d7c6a0
Olivier <olivier.gomb+github@gmail.com>
2022-05-23 17:13:58 +0200

    ajout des methodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_125306) do
+ActiveRecord::Schema.define(version: 2022_05_23_151000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "comment"
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.integer "horsepower"
+    t.integer "year"
+    t.integer "killometers"
+    t.string "plate"
+    t.string "energy"
+    t.integer "seats"
+    t.boolean "smoker"
+    t.boolean "animals"
+    t.string "color"
+    t.string "status", default: "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +56,12 @@ ActiveRecord::Schema.define(version: 2022_05_23_125306) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "cars", "users"
 end
