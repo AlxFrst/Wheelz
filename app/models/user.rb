b7d1de131b5d83a08dae429ade5b89b6f493906b
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-05-24 15:26:57 +0200

    cloudinary ça fonctionne LETS GO

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
   has_many :bookings, dependent: :destroy
   has_many :cars, through: :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
@@ -10,7 +11,12 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :username, presence: true
   validates :birthday, presence: true
-  validates :country, presence: true
   validates :city, presence: true
   validates :license, presence: true
+
+  private
+
+  def article_params
+    params.require(:user).permit(:avatar)
+  end
 end
