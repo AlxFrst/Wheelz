442b4654a6cb28344f12fbadd8c3642519fad131
Olivier <olivier.gomb+github@gmail.com>
2022-05-25 16:03:56 +0200

    correction cars details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_resource)
+    session[:previous_url] || root_path
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,
---
+++ app/javascript/channels/calendar.js
@@ -0,0 +1,7 @@
+/*const endDate = document.querySelector('#end-date')
+const startDate = document.querySelector('#start-date')
+const dateMax = startDate.value
+
+endDate.addEventListener('click', () => {
+  endDate.setAttribute('min', '2022-05-26')
+})*/
