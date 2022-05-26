c859ee7ddb50fe9b44edb23cf9e6bcf10cbf6b40
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-05-26 16:21:38 +0200

    dashboard functions

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get "dashboard-my-car", to: "profiles#my_car"
   get "dashboard-booking", to: "profiles#booking"
   delete "dashboard-booking/:id", to: "profiles#destroy_booking", as: "destroy_booking"
+  post "dashboard-booking/:id/accept", to: "profiles#accept_request", as: "accept_request"
+  post "dashboard-booking/:id/decline", to: "profiles#decline_request", as: "decline_request"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
